ae8f1c5cf7ae0494fa64267644c043cadf2a8677
ranim-arfa <ranimarfa6@gmail.com>
2026-06-25 00:52:10 +0100

    feat: enhance workshop & product forms, add settings, fix ESLint errors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const WHATSAPP_DISPLAY = "+216 93 494 954";
 
 export const SOCIAL_LINKS = {
   instagram: "https://www.instagram.com/monempreinte.tn?igsh=YXk2Z294ZzQwMjQ5",
-  facebook: "https://www.facebook.com/share/18YJNytmUb/?mibextid=wwXIfr",
+  facebook: "https://www.facebook.com/profile.php?id=61570710456603&mibextid=wwXIfr&rdid=Wwys33GzDCupGF0l&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18YJNytmUb%2F%3Fmibextid%3DwwXIfr#",
 };
 
 export const BRAND = {
